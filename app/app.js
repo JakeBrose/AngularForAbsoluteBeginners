5993d6cb0bb08f164cb0dadc28c6b35895dbc0fc
JakeBrose <mordecaistudios@gmail.com>
2018-01-30 11:53:06 -0600

    binded data from controller to view

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('Todo', [])
+'use strict';
+// This module represents the app. Currently has no dependencies.
+var app = angular.module('Todo', []);
+// adds a controller that takes 2 arguments: 
+// First, is the name of the controller.
+// Second is a function that contains the logic used by the controller.
+// $scope is the link between the controller and the view
+app.controller('TodoCtrl', function($scope){
+    // Adds a message to the scope.
+    //  Needs to be added to the page. This is called data binding.
+    $scope.message = 'Angular is pretty cool';
+});
